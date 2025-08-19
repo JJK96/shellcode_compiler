a7507fb3f141fd9c2464ad867752e5caaf6752cb
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2025-08-19 10:29:00 +0200

    Use libc crate

--- payload.rs
+++ payload.rs
@@ -2,15 +2,12 @@
 use windows_sys::{
     core::*, Win32::System::Threading::*, Win32::UI::WindowsAndMessaging::*,
 };
-
-extern "C" {
-    fn printf(format: *const u8, ...) -> i32;
-}
+use libc::*;
 
 #[no_mangle]
 fn main() -> () {
     unsafe {
         WinExec(s!("calc.exe"), SW_SHOWNORMAL.try_into().unwrap());
-        printf(s!("Test\n"));
+        printf(s!("Test\n") as *const i8);
     }
 }
