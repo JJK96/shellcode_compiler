0f0fbc571946646c29ae1ed91f906cf29ef0d949
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2025-08-19 14:27:01 +0200

    Reduce pollution in build directory

--- assets/rust/fake_linker.sh
+++ assets/rust/fake_linker.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-echo $@ > linker-args.txt
+# echo $@ > linker-args.txt
 for arg in $@; do
     if [[ "$arg" = *.payload.*.o ]]; then
         echo "Saving $arg to payload.o"
         cp "$arg" "./payload.o"
-    elif [[ "$arg" = *.o ]]; then
-        cp "$arg" "./$(basename "$arg")_saved.o"
+    # elif [[ "$arg" = *.o ]]; then
+    #     cp "$arg" "./$(basename "$arg")_saved.o"
     fi
-done > fake-linker.log
+done
+# done > fake-linker.log
